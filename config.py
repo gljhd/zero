f13d20f88614882d6a18bb181d89a9bc2665e7a7
gelj <gelj@wingspan.cn>
2018-07-25 15:54:07 +0800

    config

---
+++ config.py
@@ -0,0 +1,8 @@
+            # background;    airplane;   bicycle;    bird;   boat;   bottle;
+palette = [[128, 0, 0], [0, 128, 0], [128, 128, 0], [0, 0, 128], [128, 0, 128],
+                #bus;   car;    cat;    chair;  cow;    Dining table;
+                [0, 128, 128],[128, 128, 128], [64, 0, 0], [192, 0, 0], [64, 128, 0], [192, 128, 0],
+                #dog;   horse;  motor bike;     persion;    potted plant;   sheep;
+                [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128], [0, 64, 0], [128, 64, 0],
+                # sofa;   train;  monitor
+                [0, 192, 0], [128, 192, 0], [0, 64, 128]]
